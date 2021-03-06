c6bbc4c38f8d632874cc6a05acb865382bd1b272
Roman Bobkov <rbobkov@softailor.ru>
2021-03-06 14:20:44 +0300

    фикс ошибки с экстеншеном

--- src/Albuquerque.Infrastructure/Services/IssuesService.cs
+++ src/Albuquerque.Infrastructure/Services/IssuesService.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -7,7 +6,6 @@ using System.Threading.Tasks;
 using Albuquerque.Core.Data;
 using Albuquerque.Core.Entities;
 using Albuquerque.Core.Enums;
-using Albuquerque.Core.Extensions;
 using Albuquerque.Core.Helpers;
 using Albuquerque.Core.Models;
 using MongoDB.Bson;
